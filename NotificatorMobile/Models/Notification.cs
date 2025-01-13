c951171c8e397517fd23ef1bd00450780fc6072e
JanB558 <72133201+JanB558@users.noreply.github.com>
2025-01-13 15:50:29 +0100

    added model and empty services

---
+++ NotificatorMobile/Models/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificatorMobile.Models
+{
+    internal class Notification
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required DateTime TimeAndDate { get; set; }
+        public bool IsRecurring { get; set; }
+    }
+}

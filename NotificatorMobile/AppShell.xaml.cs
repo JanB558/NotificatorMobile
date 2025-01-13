68733b914ee575b89642eebf3c6d5b3e219c9b6d
JanB558 <72133201+JanB558@users.noreply.github.com>
2025-01-13 14:44:07 +0100

    Add project files.

---
+++ NotificatorMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotificatorMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NotificatorMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NotificatorMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

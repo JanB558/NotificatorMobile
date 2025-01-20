d0834035ffedeb8e2d2c6c11247af41378115605
JanB558 <72133201+JanB558@users.noreply.github.com>
2025-01-20 15:58:57 +0100

    wip global styles

--- NotificatorMobile/App.xaml.cs
+++ NotificatorMobile/App.xaml.cs
@@ -1,15 +1,30 @@
-﻿namespace NotificatorMobile
+﻿using CommunityToolkit.Maui.Markup;
+
+namespace NotificatorMobile
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            InitializeStyles();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
+        }
+
+        private void InitializeStyles()
+        {
+            Resources.Add("GlobalLabelStyle", new Style(typeof(Label))
+            {
+                Setters =
+                {
+                    new Setter { Property = Label.FontSizeProperty, Value = 18 },
+                    new Setter { Property = Label.FontFamilyProperty, Value = Microsoft.Maui.Font.Default }
+                }
+            });
         }
     }
 }
